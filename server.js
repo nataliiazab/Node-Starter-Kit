5ed2038fcbed658d632f527560e43a6dd6cbe34b
nataliizab <nataliia.zab@gmail.com>
2023-03-23 21:48:28 +0000

    Completed Exercise 2.1 - b and Exercise 3.1

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
 const express = require(`express`);
 const app = express();
 
+// app.get("/", function (req, res) {
+//   res.send("Yay Node!");
+// });
+
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+
+app.get("/chocolate", function (req, res) {
+  searchQuery = req.query.amount;
+  res.send(
+    "You have requested the following number of chocolate bars " + searchQuery
+  );
+});
+
+app.get("/node", function (req, res) {
+  res.send("node");
+});
+
+app.get("/codeyourfuture", function (req, res) {
+  res.send("CYF:)");
+});
+
 app.listen(3000, function () {
   console.log("Server is listening on port 3000. Ready to accept requests!");
 });
